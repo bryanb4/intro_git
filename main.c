229b3e11d669156b92ecdef38653e361fef4d1a5
bryan oregel <bryan.martinez1191@alumnos.udg.mx>
2020-09-19 23:59:22 -0500

    capturamos la edad e imrpimimos la edad +1

--- main.c
+++ main.c
@@ -2,11 +2,15 @@
 
 int main(){
     char nombre[100];
+    unsigned int edad;
 
     printf("hola mundo\n");
     printf("nombre:   ");
     fgets(nombre, sizeof(nombre), stdin);
     printf("hola %s", nombre);
+    printf("edad:  ");
+    scanf("%u", &edad);
+    printf("vas a cumplir %u", edad + 1);
 
-    return 0;
+
 }
