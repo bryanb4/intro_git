fc8281b8c7ab589101b9f318ed55ecc38887ad8f
bryan oregel <bryan.martinez1191@alumnos.udg.mx>
2020-09-24 20:02:16 -0500

    comentamos el hola mundo

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(){
     char nombre[100];
     unsigned int edad;
 
-    printf("hola mundo\n");
+   // printf("hola mundo\n");
     printf("nombre:   ");
     fgets(nombre, sizeof(nombre), stdin);
     printf("hola %s", nombre);
